31f31925783b995effa266bb9c39eef80acf8dc3
zhangqinning <zhangqinning1990@gmail.com>
2015-08-08 16:07:54 +0800

    新增数据库帮助类，以及各种表对应的实体。

---
+++ app/src/main/java/com/weather/app/weather/model/Province.java
@@ -0,0 +1,34 @@
+package com.weather.app.weather.model;
+
+/**
+ * Created by zhangqinning on 15/8/8.
+ */
+public class Province {
+    private int id ;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
